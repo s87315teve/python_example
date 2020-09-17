eaf2e39d44cc97e9a3384475274add97bb4145b0
s87315teve <s87315teve@gmail.com>
2020-09-18 02:36:32 +0800

    update TCP_server multi_sock

--- websocket_example.py
+++ websocket_example.py
@@ -8,6 +8,50 @@ import sys
 import traceback
 import argparse
 
+class multi_sock(threading.Thread):
+    def __init__(self, conn, addr, code, buffer_size, timeout):
+        threading.Thread.__init__(self)
+        self.conn=conn
+        self.addr=addr
+        self.code=code
+        self.buffer_size=buffer_size
+        self.timeout=timeout
+        print("{} connected to server".format(addr))
+        
+                    
+    def run(self):
+        if self.timeout!=None:
+            self.conn.settimeout(self.timeout)
+        while True:
+            try:
+                msg=self.conn.recv(self.buffer_size)
+                if not msg:
+                    print("client {} is gone ".format(self.addr))
+                    print("connection closed")
+                    self.conn.close()
+                    break
+                msg=msg.decode(self.code)
+                print("recv msg: ",msg)
+
+            except socket.timeout:
+                print("{} socket timeout".format(self.addr))
+                print("connection closed")
+                self.conn.close()
+                break
+            except Exception as e:
+                error_class = e.__class__.__name__ #取得錯誤類型
+                detail = e.args[0] #取得詳細內容
+                cl, exc, tb = sys.exc_info() #取得Call Stack
+                lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+                fileName = lastCallStack[0] #取得發生的檔案名稱
+                lineNum = lastCallStack[1] #取得發生的行號
+                funcName = lastCallStack[2] #取得發生的函數名稱
+                errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
+                print(errMsg)
+                self.conn.close()
+                print("{} connection closed".format(self.addr))
+                break
+
 class TCP_server():
     def __init__(self, host="127.0.0.1", port=6000, code="utf-8", buffer_size=1024, timeout=None):
         try:
@@ -17,6 +61,7 @@ class TCP_server():
             self.buffer_size=buffer_size
             self.timeout=timeout
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #建立socket
+            self.client_list=[]
         except Exception as e:
             error_class = e.__class__.__name__ #取得錯誤類型
             detail = e.args[0] #取得詳細內容
@@ -48,46 +93,15 @@ class TCP_server():
             errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
             print(errMsg)
             print("server failed to start")
-        while True:
-            conn, addr = self.sock.accept()
-            print("{} connected to server".format(addr))
-            if self.timeout!=None:
-                conn.settimeout(self.timeout)
+        try:
             while True:
-                try:
-                    msg=conn.recv(self.buffer_size)
-                    if not msg:
-                        print("client is gone ")
-                        print("connection closed")
-                        conn.close()
-                        print("sleep 1s and restart socket")
-                        time.sleep(1)
-                        break
-                    msg=msg.decode(self.code)
-                    print("recv msg: ",msg)
-
-                except socket.timeout:
-                    print("socket timeout")
-                    print("connection closed")
-                    conn.close()
-                    print("sleep 1s and restart socket")
-                    time.sleep(1)
-                    break
-                except Exception as e:
-                    error_class = e.__class__.__name__ #取得錯誤類型
-                    detail = e.args[0] #取得詳細內容
-                    cl, exc, tb = sys.exc_info() #取得Call Stack
-                    lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
-                    fileName = lastCallStack[0] #取得發生的檔案名稱
-                    lineNum = lastCallStack[1] #取得發生的行號
-                    funcName = lastCallStack[2] #取得發生的函數名稱
-                    errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
-                    print(errMsg)
-                    print("connection closed")
-                    conn.close()
-                    print("sleep 1s and restart socket")
-                    time.sleep(1)
-                    break
+                conn, addr = self.sock.accept()
+                self.client_list.append(multi_sock(conn=conn, addr=addr, code=self.code, buffer_size=self.buffer_size, timeout=self.timeout))
+                self.client_list[-1].start()
+        except Exception:
+            pass
+            
+    
 class TCP_client():
     def __init__(self, host="127.0.0.1", port=6000, code="utf-8", buffer_size=1024):
         try:
